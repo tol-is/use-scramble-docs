360814fa54a15e2a46f28e081b4963b24b5218a6
Tolis C <a7sc11u@gmail.com>
2023-12-22 00:09:49 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export const App = () => {
           <VersionLink
             major="2"
             minor="2"
-            patch="12"
+            patch="14"
             target="_blank"
             rel="noreferrer noopener"
             href="https://www.npmjs.com/package/use-scramble"
